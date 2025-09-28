6966dfe253c2b7dfa8326bb00b5c8e270dfa133f
riverm00n-03 <234728447+riverm00n-03@users.noreply.github.com>
2025-09-28 17:11:11 +0900

    reset
    flask -> fastapi로 바꾸기 위한 사전 작업

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# .env 파일의 내용을 환경변수로 로드
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key' # 플라스크 비밀 키. 
-    # 필요한 경우 차차 추가함.
--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# 플라스크 앱 생성함.
-app = create_app()
-
-if __name__ == '__main__':
-    # host='0.0.0.0'로 설정해야 Docker 컨테이너 외부에서 접속 가능
-    app.run(host='0.0.0.0', debug=True)
