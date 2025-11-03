18784d05f62baf7f444b9f89770ecb00ea10e9a8
riverm00n-03 <234728447+riverm00n-03@users.noreply.github.com>
2025-11-03 10:31:55 +0900

    Update config.py to use environment variables for database configuration

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
+import os
+
 # config.py
 
 # 데이터베이스 연결을 위한 설정 정보
 # 이 딕셔너리를 app과 admin_cli에서 모두 import하여 사용합니다.
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'password',
-    'database': 'wcheck'
+    'host': os.environ.get('DB_HOST'),
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'database': os.environ.get('DB_DATABASE')
 }
 
 # 여기에 Flask 시크릿 키 등 다른 프로젝트 설정도 추가할 수 있습니다.
